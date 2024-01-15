f5da8a014d7e6644543f7553e1512c975167137d
Usman Khalid <usmanchaudrhy@gmail.com>
2024-01-15 22:00:33 +1100

    added expo map view

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import MapView from 'react-native-maps'
+
 export default function App() {
+    /** Test application for the maps project */
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <MapView style={styles.map} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
 });

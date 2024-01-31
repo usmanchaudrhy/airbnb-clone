2c21679e4a7a07272c3f9a9d2c851e9c5b028ce4
Usman Khalid <usmanchaudrhy@gmail.com>
2024-01-31 23:34:22 +1100

    Added user page tests

--- app/_layout.js
+++ app/_layout.js
@@ -2,6 +2,7 @@ import { Slot } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function HomeLayout() {
+  // Slot is where the app body will go
   return (
     <>
       <Text style={{ backgroundColor: "red", fontSize: 40 }}>Header</Text>
--- app/user/[id].js
+++ app/user/[id].js
@@ -1,5 +1,7 @@
 import { View, Text } from "react-native";
 
+// This is the user page
+// Edit added typescript types
 export default function User() {
   return (
     <View>

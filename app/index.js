bdc37f52f86a72487af55da637a00a97ec78c6f6
Usman Khalid <usmanchaudrhy@gmail.com>
2024-02-05 22:30:29 +1100

    testing changes

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import { View, Text, Button } from "react-native";
 import { Link } from "expo-router";
 
+// Testing some changes here
 export default function Page() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Hello, World!</Text>
       <Link href="/about" asChild>
         <Button title="About" />
+
       </Link>
     </View>
   );
